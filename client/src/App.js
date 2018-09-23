366deb555f620844c6d8fd6d05436d4761d6a1f5
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2018-09-23 18:41:33 +0300

    Add grid

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
   color: white;
 `;
 
@@ -9,11 +14,20 @@ const Header = styled.h1`
   margin: 0;
 `;
 
+const Module = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 class App extends Component {
   render() {
     return (
       <Container>
-        <Header>Jee!</Header>
+        <Module>Jee!</Module>
+        <Module>Jee!</Module>
+        <Module>Jee!</Module>
+        <Module>Jee!</Module>
       </Container>
     );
   }
