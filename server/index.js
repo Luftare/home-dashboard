06983dca96a4e90b57353506abb191954cf8fcdd
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2018-09-23 11:29:18 +0300

    Define middleware before calling listen method

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const path = require('path');
 const app = express();
 const port = 8888;
 
-app.listen(port, () => console.log(`Serving at port ${port}`));
-
 app.use('/', express.static(path.join(__dirname, '../client/build')));
+
+app.listen(port, () => console.log(`Serving at port ${port}`));
