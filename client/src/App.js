32c49e8218336d7c9d862a441264716a8ea418f4
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2018-09-23 18:52:09 +0300

    Use flex instead of grid

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,13 @@ import styled from 'styled-components';
 const Container = styled.div`
   width: 100%;
   height: 100vh;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
+  display: flex;
+  flex-wrap: wrap;
   color: white;
 `;
 
-const Header = styled.h1`
-  margin: 0;
-`;
-
 const Module = styled.div`
+  width: 50%;
   display: flex;
   justify-content: center;
   align-items: center;
